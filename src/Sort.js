81a627dafc6b79d788e88731ac0747543dd45e68
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2018-01-17 19:02:15 +0900

    pass test for sort method

--- src/Sort.js
+++ src/Sort.js
@@ -1,46 +1,43 @@
+const swap = (targetArray, index1, index2) => {
+  const currentElement = targetArray[index1];
+  targetArray[index1] = targetArray[index2];
+  targetArray[index2] = currentElement;
+  return targetArray;
+};
+
+const createMaxHeap = (array) => {
+  const maxHeap = [array[0]];
+  // loop though each element
+  for (let i = 1; i < array.length; i += 1) {
+    maxHeap.push(array[i]);
+    let currentIndex = i;
+
+    while (currentIndex > 0) {
+      const parentIndex = Math.floor((currentIndex - 1) / 2);
+      if (maxHeap[currentIndex] > maxHeap[parentIndex]) {
+        swap(maxHeap, currentIndex, parentIndex);
+      }
+      currentIndex = parentIndex;
+    }
+  }
+  return maxHeap;
+};
+
 class HeapSort {
   constructor(array) {
     this.array = array;
-    this.maxHeap = [];
-  }
-
-  createMaxHeap() {
-    this.maxHeap.push(this.array[0]);
-
-    for (let i = 1; i < this.array.length; i += 1) {
-      this.maxHeap.push(this.array[i]);
-      let currentIndex = i;
-
-      while (currentIndex > 0) {
-        const parentIndex = Math.floor((currentIndex - 1) / 2);
-        
-        if (this.maxHeap[currentIndex] > this.maxHeap[parentIndex]) {
-          this.swap(this.maxHeap, currentIndex, parentIndex);
-        }
-        currentIndex = parentIndex;
-      }
-    }
-    return this.maxHeap;
-  }
-
-  swap(array, index1, index2) {
-    const currentElement = array[index1];
-    array[index1] = array[index2];
-    array[index2] = currentElement;
-    return array;
   }
 
   sort() {
-    if (this.array.length === 0) {
-      return;
+    const resultArray = [];
+    let maxHeap = this.array;
+    while (maxHeap.length > 0) {
+      maxHeap = createMaxHeap(maxHeap);
+      maxHeap = swap(maxHeap, 0, maxHeap.length - 1);
+      const elementToAdd = maxHeap.pop();
+      resultArray.unshift(elementToAdd);
     }
-    // 1. create heap with given array
-    this.createMaxHeap();
-    // 2. swap root and last in heap
-    // 3. element last element from heap
-    // 4. get fist element of the heap and push to  
-    // 5. get root value from heap and shift to array
-    // 6. back to 1. until array has no value
+    this.array = resultArray;
   }
 
   returnValue() {
@@ -48,4 +45,4 @@ class HeapSort {
   }
 }
 
-module.exports = HeapSort;
+module.exports = { HeapSort, swap, createMaxHeap };
--- test/_sort.js
+++ test/_sort.js
@@ -1,51 +1,54 @@
-const HeapSort = require('../src/Sort');
+const Sort = require('../src/Sort');
+const HeapSort = Sort.HeapSort;
+const swap = Sort.swap;
+const createMaxHeap = Sort.createMaxHeap;
 const { expect } = require('chai');
 
 describe('Sort', () => {
-  it('should be a function', () => {
-    expect(HeapSort).to.be.a('function');
-  });
-  it('should have a sort method', () => {
-    expect(HeapSort.prototype.sort).to.be.a('function');
-  });
-  
-  describe('createMaxHeap method', () => {
+  describe('createMaxHeap function', () => {
     it('should return an array of max heap', () => {
       // Setup
-      const heapSortIns = new HeapSort([4, 10, 3, 5, 1]);
       const expected = [10, 5, 3, 4, 1];
       // Exercise
-      heapSortIns.createMaxHeap();
-  
+      const actual = createMaxHeap([4, 10, 3, 5, 1]);
       // Assert
-      expect(heapSortIns.maxHeap).to.eql(expected);
+      expect(actual).to.eql(expected);
     });
   });
 
-  describe('swap method', () => {
+  describe('swap function', () => {
     it('should swap first and last element of array', () => {
       // Setup
-      const heapSortIns = new HeapSort([4, 10, 3, 5, 1]);
       const expected = [1, 10, 3, 5, 4];
       // Exercise
-      const actual = heapSortIns.swap([4, 10, 3, 5, 1], 0, 4);
+      const actual = swap([4, 10, 3, 5, 1], 0, 4);
   
       // Assert
       expect(actual).to.eql(expected);
     });
   });
 
-  describe('sort method', () => {
-    it('should return sorted array', () => {
-      // Setup
-      const heapSortIns = new HeapSort([4, 10, 3, 5, 1]);
-      const expected = [1, 3, 4, 5, 10];
-      // Exercise 
-      heapSortIns.sort();
-      const actual = heapSortIns.returnValue();
-  
-      // Assert
-      expect(actual).to.eql(expected);
+  describe('HeapSort class', () => {
+    it('should be a function', () => {
+      expect(HeapSort).to.be.a('function');
     });
-  })
+    it('should have a sort method', () => {
+      expect(HeapSort.prototype.sort).to.be.a('function');
+    });
+    
+  
+    describe('sort method', () => {
+      it('should return sorted array', () => {
+        // Setup
+        const heapSortIns = new HeapSort([4, 10, 3, 5, 1]);
+        const expected = [1, 3, 4, 5, 10];
+        // Exercise 
+        heapSortIns.sort();
+        const actual = heapSortIns.returnValue();
+    
+        // Assert
+        expect(actual).to.eql(expected);
+      });
+    });
+  });
 });
