9341d112aaa168df874ab294b046d27e535c557a
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2018-01-17 15:53:03 +0900

    create test for createMaxHeapMethod

--- test/_sort.js
+++ test/_sort.js
@@ -8,15 +8,29 @@ describe('Sort', () => {
   it('should have a sort method', () => {
     expect(HeapSort.prototype.sort).to.be.a('function');
   });
-  it('should return sorted array', () => {
+  
+  describe('createMaxHeap method', () => {
     // Setup
-    const heapSortIns = new HeapSort([4, 10, 3, 5, 1]);
-    
-    // Exercise 
-    heapSortIns.sort();
-    const expected = heapSortIns.returnValue();
+    const heapSortIns = new HeapSort();
+    const expected = [10, 5, 3, 4, 1];
+    // Exercise
+    const actual = heapSortIns.createMaxHeap([4, 10, 3, 5, 1]);
 
     // Assert
-    expect(expected).to.eql([1, 3, 4, 5, 10]);
+    expect(actual).to.eql(expected);
+  });
+
+  describe('sort method', () => {
+    it('should return sorted array', () => {
+      // Setup
+      const heapSortIns = new HeapSort([4, 10, 3, 5, 1]);
+      const expected = [1, 3, 4, 5, 10];
+      // Exercise 
+      heapSortIns.sort();
+      const actual = heapSortIns.returnValue();
+  
+      // Assert
+      expect(actual).to.eql(expected);
+    });
   })
 });
