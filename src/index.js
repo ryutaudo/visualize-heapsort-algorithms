fdbe6c7e196ce11f41e235638a708d9def9b0eb0
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2018-01-18 16:50:31 +0900

    get randome array for initialization

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const heapSort = new HeapSort(unsorted);
 const sorted = heapSort.sort();
 const maxHeight = Math.max(...unsorted);
 
+// Create Title
 function createCheesyTitle(slogan) {
   const container = document.createElement('h1');
   const textNode = document.createTextNode(slogan);
@@ -25,16 +26,9 @@ function createCheesyTitle(slogan) {
 const title = createCheesyTitle(heapSort.returnValue('Re-Engineer Yourself'));
 document.getElementById('title').appendChild(title);
 
-/*
-An simple example of how you can make your project a bit more
-interactive, if you would like.
-
-In our `index.html` page, we have a short form.
-Here is the code that talks to it.
-*/
 function changeTitle(event) {
   event.preventDefault();
-  const newTitle = createCheesyTitle(document.getElementById('form').myname.value);
+  const newTitle = createCheesyTitle(document.getElementById('name-form').myname.value);
   const titleElement = document.getElementById('title');
   titleElement.removeChild(titleElement.firstChild);
   titleElement.appendChild(newTitle);
@@ -44,6 +38,41 @@ const form = document.querySelector('form');
 document.addEventListener('DOMContentLoaded', () => {
   form.onsubmit = changeTitle;
 });
+
+// Initialization
+const getArrayButton = document.querySelector('#get-array');
+document.addEventListener('DOMContentLoaded', () => {
+  getArrayButton.onsubmit = getRandomArray;
+});
+
+const createRandomArray = (length, max) => {
+  const newArray = [];
+  for (let i = 0; i < length; i += 1) {
+    newArray.push(Math.round(Math.random() * max))
+  }
+  return newArray
+}
+
+const createBars = (array) => {
+  for(let i = 0; i < array.length; i += 1) {
+    const bar = document.createElement('div');
+    const barId = 'bar'+ i;
+    const height = array[i] / maxHeight * 300 + 'px';
+    bar.setAttribute('class', 'bar');
+    bar.setAttribute('id', barId);
+    bar.style.height = height;
+    sortWindow.append(bar);
+  }
+}
+
+const getRandomArray = (event) => {
+  event.preventDefault();
+  removeChildrenFromSortWindow();
+  const length = document.getElementById('get-array').arraylength.value;
+  const max = document.getElementById('get-array').max.value;
+  const randomArray = createRandomArray(length, max);
+  createBars(randomArray);
+}
 
 const startButton = document.querySelector('#start');
 document.addEventListener('DOMContentLoaded', () => {
