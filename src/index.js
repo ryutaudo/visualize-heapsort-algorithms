113949a4e8eea149a5c441a5c57ca9a352525f3d
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2018-01-18 14:05:11 +0900

    show bars for sorted result

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* Example Code
-      The following is just some example code for you to play around with.
-      No need to keep this---it's just some code so you don't feel too lonely.
+The following is just some example code for you to play around with.
+No need to keep this---it's just some code so you don't feel too lonely.
 */
 
 // How can we use require here if it's frontend? We can thank webpack.
@@ -8,9 +8,12 @@ const HeapSort = require('./Sort').HeapSort;
 
 // A link to our styles!
 require('./index.css');
+
+// Setup
 const unsorted = [6, 5, 3, 1, 8, 7, 2, 4]
 const heapSort = new HeapSort(unsorted);
 const sorted = heapSort.sort();
+const maxHeight = Math.max(...unsorted);
 
 function createCheesyTitle(slogan) {
   const container = document.createElement('h1');
@@ -54,9 +57,7 @@ const unsortedArray = document.createTextNode(JSON.stringify(unsorted));
 
 const initializeSort = (event) => {
   event.preventDefault();
-  const maxHeight = Math.max(...unsorted);
   for(let i = 0; i < unsorted.length; i += 1) {
-    console.log(i)
     const bar = document.createElement('div');
     const barId = 'bar'+ i;
     const height = unsorted[i] / maxHeight * 450 + 'px';
@@ -67,9 +68,21 @@ const initializeSort = (event) => {
   }
 }
 
+const removeChildrenFromSortWindow = () => {
+  while(sortWindow.firstChild) {
+    sortWindow.removeChild(sortWindow.firstChild);
+  }  
+}
+
 const sort = (event) => {
   event.preventDefault();
-  const sortedArray = document.createTextNode(JSON.stringify(sorted));
-  unsortedArray.remove();
-  sortWindow.append(sortedArray);
+  removeChildrenFromSortWindow();
+  for(let i = 0; i < sorted.length; i += 1) {
+    const bar = document.createElement('div');
+    const barId = 'bar'+ i;
+    const height = sorted[i] / maxHeight * 450 + 'px';
+    bar.setAttribute('class', 'bar '+ barId);
+    bar.style.height = height;
+    sortWindow.append(bar);
+  }
 }
